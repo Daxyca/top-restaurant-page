459b955ceaae8d7798af8e608875b765615372af
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-09-01 10:50:59 +0800

    fix: remove redundant page navigation

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ import homePage from "./pages/home";
 import menuPage from "./pages/menu";
 import aboutPage from "./pages/about";
 
+let currentPage;
+
 function loadPage(page) {
-    if (typeof page === "function") {
-        page = page()
-    }
+    if (currentPage === page) { return }
+    const pageElement = typeof page === "function" ? page() : page;
     const content = document.getElementById("content");
-    content.textContent = ""
-    content.appendChild(page)
+    content.textContent = "";
+    content.appendChild(pageElement);
+    currentPage = page;
 }
 
-loadPage(homePage)
+loadPage(homePage);
 
-const homeBtn = document.getElementById("home-button")
-const menuBtn = document.getElementById("menu-button")
-const aboutBtn = document.getElementById("about-button")
+const homeBtn = document.getElementById("home-button");
+const menuBtn = document.getElementById("menu-button");
+const aboutBtn = document.getElementById("about-button");
 
-homeBtn.addEventListener("click", () => loadPage(homePage))
-menuBtn.addEventListener("click", () => loadPage(menuPage))
-aboutBtn.addEventListener("click", () => loadPage(aboutPage))
+homeBtn.addEventListener("click", () => loadPage(homePage));
+menuBtn.addEventListener("click", () => loadPage(menuPage));
+aboutBtn.addEventListener("click", () => loadPage(aboutPage));
